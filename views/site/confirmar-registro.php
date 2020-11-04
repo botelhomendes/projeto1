b60c1daa85f0e88d93c679b5bec43609b86c8721
moisesufop <moises2006@gmail.com>
2020-11-04 18:22:52 -0300

    inicio
    inicio do projeto

--- views/site/confirmar-registro.php
+++
@@ -1,8 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-<ul>
-    <li><label>Você </label> <?= Html::encode($model->nm_cliente) ?></li>
-    <li><label>Data de nascimento</label>: <?= Html::encode($model->dt_nascimento) ?></li>
-    <li><label>Telefone</label>: <?= Html::encode($model->nr_telefone_1) ?></li>
-</ul>
