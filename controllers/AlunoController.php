87ae78e4aa39d5708f84b6d78849dd1cebb4fe69
botelhomendes <74937527+botelhomendes@users.noreply.github.com>
2020-11-28 18:59:29 -0300

    componente data

--- controllers/AlunoController.php
+++ controllers/AlunoController.php
@@ -9,25 +9,28 @@ use app\models\AlunoSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use kartik\date\DatePicker;
+use yii\db\ActiveRecord;
 
 /**
  * AlunoController implements the CRUD actions for Aluno model.
  */
-class AlunoController extends Controller
-{
-      public $model;
+class AlunoController extends Controller {
+
+    public $model;
+
     /**
      * {@inheritdoc}
      */
-    public function behaviors()
-    {
+    public function behaviors() {
+        $model = new Aluno();
         return [
             'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'delete' => ['POST'],
-                ],
-            ],
+                         'class' => VerbFilter::className(),
+                          'actions' => [
+                            'delete' => ['POST'],
+                           ],
+                       ],          
         ];
     }
 
@@ -35,17 +38,15 @@ class AlunoController extends Controller
      * Lists all Aluno models.
      * @return mixed
      */
-    public function actionIndex()
-    {
-      //nao existe o model aqui. Mas nao precisa, é so usar o searchModel, porque ele extende de ALUNO (que é o $model).
-        
-         //blz? ok
+    public function actionIndex() {
+        //nao existe o model aqui. Mas nao precisa, é so usar o searchModel, porque ele extende de ALUNO (que é o $model).
+        //blz? ok
         $searchModel = new AlunoSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,           
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -55,10 +56,9 @@ class AlunoController extends Controller
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         return $this->render('view', [
-            'model' => $this->findModel($id),
+                    'model' => $this->findModel($id),
         ]);
     }
 
@@ -67,25 +67,22 @@ class AlunoController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
+    public function actionCreate() {
         $model = new Aluno();
         $searchModel = new AlunoSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id]);
         }
 
         return $this->render('create', [
-            'model' => $model,
-             'dataProvider' => $dataProvider,
-            'searchModel' => $searchModel,
+                    'model' => $model,
+                    'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
         ]);
     }
-    
-       public function actionRelatorio()
-    {
+
+    public function actionRelatorio() {
         $model = new Aluno();
         $searchModel = new AlunoSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
@@ -95,15 +92,13 @@ class AlunoController extends Controller
         }
 
         return $this->render('relatorio', [
-            'model' => $model,
-            'dataProvider' => $dataProvider,
-            'searchModel' => $searchModel,
+                    'model' => $model,
+                    'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
         ]);
     }
-    
 
-        public function actionTeste()
-    {
+    public function actionTeste() {
         $model = new Aluno();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
@@ -111,9 +106,10 @@ class AlunoController extends Controller
         }
 
         return $this->render('teste', [
-            'model' => $model,
+                    'model' => $model,
         ]);
     }
+
     /**
      * Updates an existing Aluno model.
      * If update is successful, the browser will be redirected to the 'view' page.
@@ -121,8 +117,7 @@ class AlunoController extends Controller
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
@@ -130,7 +125,7 @@ class AlunoController extends Controller
         }
 
         return $this->render('update', [
-            'model' => $model,
+                    'model' => $model,
         ]);
     }
 
@@ -141,8 +136,7 @@ class AlunoController extends Controller
      * @return mixed
      * @throws NotFoundHttpException if the model cannot be found
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
@@ -155,13 +149,12 @@ class AlunoController extends Controller
      * @return Aluno the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
+    protected function findModel($id) {
         if (($model = Aluno::findOne($id)) !== null) {
             return $model;
         }
 
         throw new NotFoundHttpException('The requested page does not exist.');
     }
- 
+
 }
