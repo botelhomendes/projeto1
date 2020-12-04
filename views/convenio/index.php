2647d12a8fd6b5b1989940bf8d41ab47f073c127
botelhomendes <74937527+botelhomendes@users.noreply.github.com>
2020-12-04 19:14:57 -0300

    ajustes tela

--- views/convenio/index.php
+++ views/convenio/index.php
@@ -8,6 +8,7 @@ use yii\grid\GridView;
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'Pesquisar Convênio';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="convenio-index">
 
